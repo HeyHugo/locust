f77e14167b04fcd504aae53163e81e0810fdaa94
Hugo Heyman <hugo.heyman@gmail.com>
2011-09-01 18:09:12 +0200

    removed a function I used for testing

--- locust/web.py
+++ locust/web.py
@@ -67,15 +67,6 @@ def stop():
     return response
 
 
-# Adding locust
-@app.route("/add")
-def add():
-    from core import locust_runner
-
-    locust_runner.start_hatching(5, 1)
-    return "hejsan!"
-
-
 @app.route("/stats/requests/csv")
 def request_stats_csv():
     from core import locust_runner
