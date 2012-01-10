a9814fc0111c9976826747a62a3f9c27efad9224
Jonatan Heyman <jonatan@heyman.info>
2012-01-10 22:43:11 +0100

    Refactored stats_logger into a general console logger that resides in log.py

--- locust/stats.py
+++ locust/stats.py
@@ -3,20 +3,9 @@ import gevent
 from copy import copy
 import math
 
-from exception import InterruptLocust
 import events
-
-# set up logger for the statistics tables
-import logging
-
-stats_logger = logging.getLogger("stats_logger")
-# create console handler
-sh = logging.StreamHandler()
-sh.setLevel(logging.INFO)
-# formatter that doesn't include anything but the message
-sh.setFormatter(logging.Formatter("%(message)s"))
-stats_logger.addHandler(sh)
-stats_logger.propagate = False
+from exception import InterruptLocust
+from log import console_logger
 
 STATS_NAME_WIDTH = 60
 
@@ -376,11 +365,11 @@ events.slave_report += on_slave_report
 
 
 def print_stats(stats):
-    stats_logger.info(
+    console_logger.info(
         (" %-" + str(STATS_NAME_WIDTH) + "s %7s %12s %7s %7s %7s  | %7s %7s")
         % ("Name", "# reqs", "# fails", "Avg", "Min", "Max", "Median", "req/s")
     )
-    stats_logger.info("-" * (80 + STATS_NAME_WIDTH))
+    console_logger.info("-" * (80 + STATS_NAME_WIDTH))
     total_rps = 0
     total_reqs = 0
     total_failures = 0
@@ -389,15 +378,15 @@ def print_stats(stats):
         total_rps += r.current_rps
         total_reqs += r.num_reqs
         total_failures += r.num_failures
-        stats_logger.info(r)
-    stats_logger.info("-" * (80 + STATS_NAME_WIDTH))
+        console_logger.info(r)
+    console_logger.info("-" * (80 + STATS_NAME_WIDTH))
 
     try:
         fail_percent = (total_failures / float(total_reqs)) * 100
     except ZeroDivisionError:
         fail_percent = 0
 
-    stats_logger.info(
+    console_logger.info(
         (" %-" + str(STATS_NAME_WIDTH) + "s %7d %12s %42.2f")
         % (
             "Total",
@@ -406,12 +395,12 @@ def print_stats(stats):
             total_rps,
         )
     )
-    stats_logger.info("")
+    console_logger.info("")
 
 
 def print_percentile_stats(stats):
-    stats_logger.info("Percentage of the requests completed within given times")
-    stats_logger.info(
+    console_logger.info("Percentage of the requests completed within given times")
+    console_logger.info(
         (" %-" + str(STATS_NAME_WIDTH) + "s %8s %6s %6s %6s %6s %6s %6s %6s %6s %6s")
         % (
             "Name",
@@ -427,17 +416,17 @@ def print_percentile_stats(stats):
             "100%",
         )
     )
-    stats_logger.info("-" * (80 + STATS_NAME_WIDTH))
+    console_logger.info("-" * (80 + STATS_NAME_WIDTH))
     complete_list = []
     for key in sorted(stats.iterkeys()):
         r = stats[key]
         if r.response_times:
-            stats_logger.info(r.percentile())
+            console_logger.info(r.percentile())
             complete_list.extend(r.create_response_times_list())
-    stats_logger.info("-" * (80 + STATS_NAME_WIDTH))
+    console_logger.info("-" * (80 + STATS_NAME_WIDTH))
     complete_list.sort()
     if complete_list:
-        stats_logger.info(
+        console_logger.info(
             (
                 " %-"
                 + str(STATS_NAME_WIDTH)
@@ -457,17 +446,17 @@ def print_percentile_stats(stats):
                 complete_list[-1],
             )
         )
-    stats_logger.info("")
+    console_logger.info("")
 
 
 def print_error_report():
-    stats_logger.info("Error report")
-    stats_logger.info(" %-18s %-100s" % ("# occurences", "Error"))
-    stats_logger.info("-" * (80 + STATS_NAME_WIDTH))
+    console_logger.info("Error report")
+    console_logger.info(" %-18s %-100s" % ("# occurences", "Error"))
+    console_logger.info("-" * (80 + STATS_NAME_WIDTH))
     for error, count in RequestStats.errors.iteritems():
-        stats_logger.info(" %-18i %-100s" % (count, error))
-    stats_logger.info("-" * (80 + STATS_NAME_WIDTH))
-    stats_logger.info("")
+        console_logger.info(" %-18i %-100s" % (count, error))
+    console_logger.info("-" * (80 + STATS_NAME_WIDTH))
+    console_logger.info("")
 
 
 def stats_printer():
