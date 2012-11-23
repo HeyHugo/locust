0e8e98807f602f7b9784c03ed013b8125f3f6bdd
Jonatan Heyman <jonatan@heyman.info>
2012-11-23 17:30:38 +0100

    Fixed bug with HttpSession's config not getting set

--- locust/test/runtests.py
+++ locust/test/runtests.py
@@ -12,6 +12,7 @@ from test_stats import (
 )
 from test_runners import TestMasterRunner
 from test_taskratio import TestTaskRatio
+from test_client import TestHttpSession
 
 if __name__ == "__main__":
     unittest.main()
