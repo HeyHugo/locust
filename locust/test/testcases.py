204df30cac928a0c9629bbab55291b0fe16ddc18
Jonatan Heyman <jonatan@heyman.info>
2015-03-28 11:55:28 +0100

    Added test and docs for #267 which fixes that requests got incorrectly marked as GET requests, if they resulted in a redirect

--- locust/test/testcases.py
+++ locust/test/testcases.py
@@ -85,7 +85,7 @@ def failed_request():
     return "This response failed", 500
 
 
-@app.route("/redirect")
+@app.route("/redirect", methods=["GET", "POST"])
 def do_redirect():
     delay = request.args.get("delay")
     if delay:
