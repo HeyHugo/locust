d200f5a36a8ef506c198832799bb890ae431298d
Justin Iso <justin.james.iso@gmail.com>
2016-05-30 21:36:30 -0700

    explicitly declare pyzmq to avoid using python socket implementation

--- setup.py
+++ setup.py
@@ -49,6 +49,7 @@ setup(
         "requests>=2.9.1",
         "msgpack-python>=0.4.2",
         "six>=1.10.0",
+        "pyzmq==15.2.0",
     ],
     tests_require=["unittest2", "mock", "pyzmq"],
     entry_points={"console_scripts": ["locust = locust.main:main",]},
