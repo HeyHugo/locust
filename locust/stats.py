ea87b8321ee5fdc918c3653ea792207812caa23a
git mrsanders <git@e107321-mac.cambridge.arm.com>
2016-07-28 15:44:01 +0100

    Python 3 compatibility with --no-web option
    Fixes logging during locust hatching when using --no-web with Python 3.
    (previously a "ImportError: No module named 'runners'" error is raised and no logging occurs)

--- locust/stats.py
+++ locust/stats.py
@@ -582,7 +582,7 @@ def print_error_report():
 
 
 def stats_printer():
-    from runners import locust_runner
+    from .runners import locust_runner
 
     while True:
         print_stats(locust_runner.request_stats)
