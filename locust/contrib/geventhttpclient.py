27b2ab1bfa81a851b6cbf4a3e456b0c5af7a7d57
Jonatan Heyman <jonatan@heyman.info>
2017-12-19 17:17:19 +0100

    Python 2 fixes

--- locust/contrib/geventhttpclient.py
+++ locust/contrib/geventhttpclient.py
@@ -1,5 +1,21 @@
+from __future__ import absolute_import
+
+import errno
+import six
+import socket
 from time import time
-from http.cookiejar import CookieJar
+
+if six.PY2:
+    from cookielib import CookieJar
+
+    class ConnectionRefusedError(Exception):
+        # ConnectionRefusedError doesn't exist in python 2, so we'll
+        # define a dummy class to avoid a NameError
+        pass
+
+
+else:
+    from http.cookiejar import CookieJar
 
 from geventhttpclient.useragent import (
     UserAgent,
@@ -70,7 +86,7 @@ class GeventHttpSession(object):
 
         try:
             response = self.client.urlopen(url, method=method, **kwargs)
-        except (ConnectionError, ConnectionRefusedError) as e:
+        except (ConnectionError, ConnectionRefusedError, socket.error) as e:
             # record the consumed time
             request_meta["response_time"] = int(
                 (time() - request_meta["start_time"]) * 1000
