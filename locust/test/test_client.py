3db672879fef044d0cc2e86782496db6455d12ca
vladimirs@tradeix.com <vs-odessa@yandex.ru>
2019-11-09 01:27:15 +0200

    fix unit test

--- locust/test/test_client.py
+++ locust/test/test_client.py
@@ -27,7 +27,7 @@ class TestHttpSession(WebserverTestCase):
     def test_wrong_url(self):
         for url, exception in (
             (u"http://\x94", InvalidURL),
-            ("telnet://127.0.0.1", InvalidSchema),
+            ("telnet://127.0.0.1", MissingSchema),
             ("127.0.0.1", MissingSchema),
         ):
             s = HttpSession(url)
