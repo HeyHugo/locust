50c9f8b38ed45ba797dcc0217ac3728e6155d401
Jonatan Heyman <jonatan@heyman.info>
2019-11-19 16:34:39 +0100

    Add Message.__repr__ for better debugging

--- locust/rpc/protocol.py
+++ locust/rpc/protocol.py
@@ -7,6 +7,9 @@ class Message(object):
         self.data = data
         self.node_id = node_id
 
+    def __repr__(self):
+        return "<Message %s:%s>" % (self.type, self.node_id)
+
     def serialize(self):
         return msgpack.dumps((self.type, self.data, self.node_id))
 
