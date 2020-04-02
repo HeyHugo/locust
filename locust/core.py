84f8bc2785548158ac7c29cb25b1879314a4a727
Jonatan Heyman <jonatan@heyman.info>
2020-04-02 17:13:11 +0200

    Fix issue where Locust user would sleep even when state was set to LOCUST_STATE_STOPPING if TaskSet.interrupt() were called with reschedule argument set to False.

--- locust/core.py
+++ locust/core.py
@@ -293,14 +293,9 @@ class TaskSet(object, metaclass=TaskSetMeta):
                     self.schedule_task(self.get_next_task())
 
                 try:
-                    if self.locust._state == LOCUST_STATE_STOPPING:
-                        raise StopLocust()
+                    self._check_stop_condition()
                     self.execute_next_task()
-                    if self.locust._state == LOCUST_STATE_STOPPING:
-                        raise StopLocust()
                 except RescheduleTaskImmediately:
-                    if self.locust._state == LOCUST_STATE_STOPPING:
-                        raise StopLocust()
                     pass
                 except RescheduleTask:
                     self.wait()
@@ -386,12 +381,17 @@ class TaskSet(object, metaclass=TaskSetMeta):
             )
 
     def wait(self):
+        self._check_stop_condition()
         self.locust._state = LOCUST_STATE_WAITING
         self._sleep(self.wait_time())
         self.locust._state = LOCUST_STATE_RUNNING
 
     def _sleep(self, seconds):
         gevent.sleep(seconds)
+
+    def _check_stop_condition(self):
+        if self.locust._state == LOCUST_STATE_STOPPING:
+            raise StopLocust()
 
     def interrupt(self, reschedule=True):
         """
