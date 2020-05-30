553e74715657b9ade8ddad05cfdfca200a7655b6
Lars Holmberg <lars.holmberg@svenskaspel.se>
2020-05-30 15:39:21 +0200

    Improve documentation for FastHttp & add properties to example code.

--- examples/fast_http_locust.py
+++ examples/fast_http_locust.py
@@ -2,16 +2,6 @@ from locust import HttpUser, TaskSet, task, between
 from locust.contrib.fasthttp import FastHttpUser
 
 
-class UserTasks(TaskSet):
-    @task
-    def index(self):
-        self.client.get("/")
-
-    @task
-    def stats(self):
-        self.client.get("/stats/requests")
-
-
 class WebsiteUser(FastHttpUser):
     """
     User class that does requests to the locust web server running on localhost,
@@ -20,4 +10,17 @@ class WebsiteUser(FastHttpUser):
 
     host = "http://127.0.0.1:8089"
     wait_time = between(2, 5)
-    tasks = [UserTasks]
+    # some things you can configure on FastHttpUser
+    # connection_timeout = 60.0
+    # insecure = True
+    # max_redirects = 5
+    # max_retries = 1
+    # network_timeout = 60.0
+
+    @task
+    def index(self):
+        self.client.get("/")
+
+    @task
+    def stats(self):
+        self.client.get("/stats/requests")
